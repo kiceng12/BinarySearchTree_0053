d8de04f16dbe08f01673169dd8e31044c9dc89c1
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-14 15:17:18 +0700

    menulis if

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -35,6 +35,14 @@ class BinaryTree
 
 	    Node* parent = NULL;
         Node* currentNode = NULL;
+        search(element, parent, currentNode);   // Locate the node which will be the parent of the node to be inserted
+
+        if (parent == nullptr) // If the parent is NULL (Tree is empty)
+		{
+            ROOT = newNode;  // mark the new node as the root
+	        return;  // exit
+        }
+
     }
 
     
